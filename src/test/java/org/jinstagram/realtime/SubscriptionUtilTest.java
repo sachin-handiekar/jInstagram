c3ceb3753c7487201e280ad95f10b47cfbd8b52c
Stéphane Nicoll <stephane@nicoll.net>
2014-02-22 15:38:46 +0100

    Restore Java6 compatibility
    This commit restores Java6 compatibility as only one helper class is
    used and can easily be replaced. This allows to use jInstagram in more
    environments.

--- src/test/java/org/jinstagram/realtime/SubscriptionUtilTest.java
+++ src/test/java/org/jinstagram/realtime/SubscriptionUtilTest.java
@@ -1,7 +1,7 @@
 package org.jinstagram.realtime;
 
 
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 
 import org.jinstagram.exceptions.InstagramException;
 import org.jinstagram.realtime.SubscriptionUtil.VerificationResult;
@@ -16,7 +16,8 @@ public class SubscriptionUtilTest {
 		String jsonResponse = "[{\"subscription_id\":\"1\",\"object\":\"user\",\"object_id\":\"1234\",\"changed_aspect\":\"media\",\"time\":1297286541},{\"subscription_id\":\"2\",\"object\":\"tag\",\"object_id\":\"nofilter\",\"changed_aspect\":\"media\",\"time\":1297286541}]";
 		String xHubSignature = "53a41d80a55a9265fc72633d432e22e6dc05fd64";
 		
-		VerificationResult result = SubscriptionUtil.verifySubscriptionPostRequestSignature(clientSecret, jsonResponse.getBytes(StandardCharsets.UTF_8), xHubSignature);
+		VerificationResult result = SubscriptionUtil.verifySubscriptionPostRequestSignature(clientSecret,
+              jsonResponse.getBytes(Charset.forName("UTF-8")), xHubSignature);
 		Assert.assertTrue(result.isSuccess());
 	}
 }
