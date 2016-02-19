112faa89cf5a199a62d1b8eed7debeb7abac2785
Yousef <yousefdardiry@gmail.com>
2016-02-19 13:49:39 +0100

    fix parsing access_token
    now parses access_token with gson. The regex didn't properly parse all
    instagram responses

--- src/main/java/org/jinstagram/auth/model/Constants.java
+++ src/main/java/org/jinstagram/auth/model/Constants.java
@@ -9,6 +9,5 @@ public class Constants {
 	public static final String AUTHORIZE_URL = INSTAGRAM_OAUTH_URL_BASE + "/authorize/?client_id=%s&redirect_uri=%s&response_type=code";
 
 	public static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL	+ "&scope=%s";
-	
-	public static final String ACCESS_TOKEN_EXTRACTOR_REGEX = "\"access_token\":\"(\\S*?)\"";
+
 }
