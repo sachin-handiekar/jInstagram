a5da1d8b3d056d5f2bff64a472b087ddae213154
George Kankava <george.kankava@aurea.com>
2016-03-03 14:13:20 +0400

    multiple code improvements: squid:S2325, squid:EmptyStatementUsageCheck, squid:S00105

--- src/main/java/org/jinstagram/InstagramConfig.java
+++ src/main/java/org/jinstagram/InstagramConfig.java
@@ -4,57 +4,57 @@ import org.jinstagram.model.Constants;
 
 public class InstagramConfig {
 
-	private String baseURI;
-	private String version;
-	private String apiURL;
-	private int connectionTimeoutMills = 0;
-	private int readTimeoutMills = 0;
+    private String baseURI;
+    private String version;
+    private String apiURL;
+    private int connectionTimeoutMills = 0;
+    private int readTimeoutMills = 0;
     private boolean connectionKeepAlive = false;
-	private boolean enforceSignedRequest = false;
+    private boolean enforceSignedRequest = false;
 
-	public InstagramConfig() {
-		baseURI = Constants.BASE_URI;
-		version = Constants.VERSION;
-		apiURL = Constants.API_URL;
-	}
+    public InstagramConfig() {
+        baseURI = Constants.BASE_URI;
+        version = Constants.VERSION;
+        apiURL = Constants.API_URL;
+    }
 
-	public String getBaseURI() {
-		return baseURI;
-	}
+    public String getBaseURI() {
+        return baseURI;
+    }
 
-	public void setBaseURI(String baseURI) {
-		this.baseURI = baseURI;
-		apiURL = String.format("%s/%s", baseURI, version);
-	}
+    public void setBaseURI(String baseURI) {
+        this.baseURI = baseURI;
+        apiURL = String.format("%s/%s", baseURI, version);
+    }
 
-	public String getVersion() {
-		return version;
-	}
+    public String getVersion() {
+        return version;
+    }
 
-	public void setVersion(String version) {
-		this.version = version;
-		apiURL = String.format("%s/%s", baseURI, version);
-	}
+    public void setVersion(String version) {
+        this.version = version;
+        apiURL = String.format("%s/%s", baseURI, version);
+    }
 
-	public String getApiURL() {
-		return apiURL;
-	}
+    public String getApiURL() {
+        return apiURL;
+    }
 
-	public int getConnectionTimeoutMills() {
-		return connectionTimeoutMills;
-	}
+    public int getConnectionTimeoutMills() {
+        return connectionTimeoutMills;
+    }
 
-	public void setConnectionTimeoutMills(int connectionTimeoutMills) {
-		this.connectionTimeoutMills = connectionTimeoutMills;
-	}
+    public void setConnectionTimeoutMills(int connectionTimeoutMills) {
+        this.connectionTimeoutMills = connectionTimeoutMills;
+    }
 
-	public int getReadTimeoutMills() {
-		return readTimeoutMills;
-	}
+    public int getReadTimeoutMills() {
+        return readTimeoutMills;
+    }
 
-	public void setReadTimeoutMills(int readTimeoutMills) {
-		this.readTimeoutMills = readTimeoutMills;
-	}
+    public void setReadTimeoutMills(int readTimeoutMills) {
+        this.readTimeoutMills = readTimeoutMills;
+    }
 
     public boolean isConnectionKeepAlive() {
         return connectionKeepAlive;
@@ -64,11 +64,11 @@ public class InstagramConfig {
         this.connectionKeepAlive = connectionKeepAlive;
     }
 
-	public boolean isEnforceSignedRequest() {
-		return enforceSignedRequest;
-	}
+    public boolean isEnforceSignedRequest() {
+        return enforceSignedRequest;
+    }
 
-	public void setEnforceSignedRequest(boolean enforceSignedRequest) {
-		this.enforceSignedRequest = enforceSignedRequest;
-	}
+    public void setEnforceSignedRequest(boolean enforceSignedRequest) {
+        this.enforceSignedRequest = enforceSignedRequest;
+    }
 }
